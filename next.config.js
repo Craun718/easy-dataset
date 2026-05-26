ea8a0dda9aee2f41b61a092349f31e75aba43665
Craun718 <3653544699@qq.com>
2026-05-26 09:01:11 +0800

    refactor: migrate to ESM and upgrade core dependencies

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
-// 最佳实践配置示例
-module.exports = {
-  experimental: {
-    serverComponentsExternalPackages: ['@opendocsg/pdf2md', 'pdfjs-dist', '@hyzyla/pdfium'],
-    esmExternals: 'loose'
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.externals.push({
-        unpdf: 'window.unpdf',
-        'pdfjs-dist': 'window.pdfjsLib'
-      });
-    } else {
-      config.externals.push('pdfjs-dist');
-      config.externals.push('@hyzyla/pdfium');
-    }
-    return config;
-  }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  serverExternalPackages: ['@opendocsg/pdf2md', '@hyzyla/pdfium'],
 };
+
+module.exports = nextConfig;
